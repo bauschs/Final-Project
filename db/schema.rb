fda380e8e0c705f63578fa784d26e98d537d24d3
Stephen Bausch <bauschs@unlv.nevada.edu>
2014-11-24 07:52:15 +0000

    eventsfinish

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141110215050) do
+ActiveRecord::Schema.define(version: 20141120141346) do
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "locations", force: true do |t|
+    t.string   "name"
+    t.string   "tag"
+    t.integer  "event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "locations", ["event_id"], name: "index_locations_on_event_id"
 
   create_table "qrcs", force: true do |t|
     t.string   "code"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+2.times do |i|
+  Event.create(name: "Event #{i + 1}")
+  3.times do |j|
+    if Location.any?
+      id = Location.last.id
+    else
+      id = 0
+    end
+    Location.create(
+      name: "Location #{id + 1} belongs to Event #{i + 1}",
+      tag: (('A'..'Z').to_a + ('a'..'z').to_a + (0..9).to_a).shuffle[0..7].join,
+      event_id: i + 1
+    )
+  end
+end
